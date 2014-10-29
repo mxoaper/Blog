497cfd9768f374f2d3e6d4210ac9e79d5a6a6537
Mikael Nortikyan <nortikyan99@gmail.com>
2014-10-28 23:13:48 -0700

    Comments
    I've commented out my code for create-db.php

--- Controller/create-db.php
+++ Controller/create-db.php
@@ -1,22 +1,22 @@
 <?php  
-
+ // Check if database.php has already been included, and if not, to require it.
  require_once(__DIR__ . "/../model/database.php");
-
+ // Opens a new connection to the MySQL server
  $connection = new mysqli($host, $username, $password);
-
+ // Checks if their is an error, if so, echos it out.
  if ($connection->connect_error) {
  	die("Error: " . $connection->connect_error);
  }
-
+  // Selects the database
   $exists = $connection->select_db($database);
     
    if (!$exists) {
-
+   	// Sends commands to the Databases
    	$query = $connection->query("CREATE DATABASE $database");
-
+    // Checks if the database has been created and outputs and echo saying so.
    	if ($query) {
 	  echo "Successfully created a database: " . $database;     
    	}
    }
-
+ // Closes the connection
  $connection->close();

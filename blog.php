854cca43f09b45941f3f6f2f06b2666df8cd92c2
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-02 18:47:21 -0800

    Commented out my code
    I commented out what my code for "    require_once(__DIR__ . "/view/header.php");" does.

--- blog.php
+++ blog.php
@@ -1,4 +1,5 @@
 <?php
+// Requires header.php in the blog
 	require_once(__DIR__ . "/view/header.php");
 // Checks if the fille is already their. If not, requires it.
  require_once(__DIR__ . "/controller/create-db.php");

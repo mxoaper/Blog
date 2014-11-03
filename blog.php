78086366d322f812de0e069fe139a67b50b8473b
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-02 19:15:29 -0800

    Requires my navigation.php in Blog
    I've used require_once to require my navigation.php in my Blog.php

--- blog.php
+++ blog.php
@@ -1,6 +1,7 @@
 <?php
 // Requires header.php in the blog
 	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/view/navigation.php");
 // Checks if the fille is already their. If not, requires it.
  require_once(__DIR__ . "/controller/create-db.php");
  // Requires footer.php in the blog

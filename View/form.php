6bc700066bcb33d6addff07a0a63f6a39bb0a053
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-02 23:07:35 -0800

    Commented out my code
    I commented out my recently added code for others to be able to clearly see it.

--- View/form.php
+++ View/form.php
@@ -1,13 +1,19 @@
 <h1>Create Blog Post</h1>
 
+<!-- Creates a form -->
 <form>
+<!-- Needs 2 Text Boxs (2 Divs) -->
  <div>
+ <!-- Will be able to input text for what the Title will be  -->
  	<label for="title">Title: </label>
+ <!-- Will be able to type text into this input box; title -->
  	<input type = "text" name = "title" />
  </div>
 
  <div>
+ <!-- The label/name for the div; Post -->
  	<label for="post">Post:</label>
+ <!-- The area for the user to type in what they want to say -->
  	<textarea name="post"></textarea>
  </div>
 </form>

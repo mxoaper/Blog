d7be3e7a14df345175b370fc87269148392e51c2
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-03 21:38:30 -0800

    Established a Database Connection
    I established a connection to my database and ended the connection at the very end of create-post.php

--- Controller/create-post.php
+++ Controller/create-post.php
@@ -2,8 +2,12 @@
 // Requires databse.php inside this file
 	require_once(__DIR__ . "/../model/database.php");
 
+	$connection = new mysqli($host, $username, $password, $databse);
+
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
 	echo "<p> Title: $title</p>";
 	echo "<p> Post: $post</p>";
+
+	$connection->close();

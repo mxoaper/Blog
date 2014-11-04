ccb631d0b18945488ad1bc86fee5bf0fd985a27a
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-04 12:24:10 -0800

    Commented out my code
    I've commented out my recently added/modified code to clarify what code does what.

--- Controller/create-post.php
+++ Controller/create-post.php
@@ -4,14 +4,19 @@
 // Established a connection to my database.php
 	$connection = new mysqli($host, $username, $password, $database);
 
+// Makes sure there is no malicous codes trying to hack my database through the title
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+// Makes sure there is no malicous codes trying to hack my database through the post
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
+// Made a query which inserts the post and sets the title of the post into my database
 	$query = $connection->query("INSERT INTO posts SET title = '$title' , post = '$post'");
 
+// If Succesfull at submitting post, echoes out its success
 	if ($query) {
 		echo "<p>Successfully inserted post: $title</p>";
 	}
+// If not successfull at submitting the post, it will echo out the error
 	else {
 		echo "<p>$connection->error</p>";
 	}

fa0e3ef82124a5cb7e6893c1ff166819856bb269
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-03 21:30:25 -0800

    Required my Database in Create-post.php
    I required my database.php inside my create-post.php using require_once .

--- Controller/create-post.php
+++ Controller/create-post.php
@@ -1,4 +1,5 @@
 <?php 
+	require_once(__DIR__ . "/../model/database.php");
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 

e05d7cd7b9826764a2842f3789afb4c5f464b7af
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-03 21:41:02 -0800

    Commented out my code
    I commented out my recently added code to clarify what it does.

--- Controller/create-post.php
+++ Controller/create-post.php
@@ -1,7 +1,7 @@
 <?php 
 // Requires databse.php inside this file
 	require_once(__DIR__ . "/../model/database.php");
-
+// Established a connection to my database.php
 	$connection = new mysqli($host, $username, $password, $databse);
 
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
@@ -9,5 +9,5 @@
 
 	echo "<p> Title: $title</p>";
 	echo "<p> Post: $post</p>";
-
+// Closed my connection to my databse
 	$connection->close();

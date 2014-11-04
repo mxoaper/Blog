d8f6fba31ed8763ebe43271837849b4b391d5479
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-04 12:19:53 -0800

    Able to Submit Blog Posts and store them in the database
    I've made a query which inserts the post and title into my database. Once successful at doing so, it echoes out a message stating its success! If not successful, it echoes out its error.

--- Controller/create-post.php
+++ Controller/create-post.php
@@ -2,12 +2,19 @@
 // Requires databse.php inside this file
 	require_once(__DIR__ . "/../model/database.php");
 // Established a connection to my database.php
-	$connection = new mysqli($host, $username, $password, $databse);
+	$connection = new mysqli($host, $username, $password, $database);
 
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-	echo "<p> Title: $title</p>";
-	echo "<p> Post: $post</p>";
+	$query = $connection->query("INSERT INTO posts SET title = '$title' , post = '$post'");
+
+	if ($query) {
+		echo "<p>Successfully inserted post: $title</p>";
+	}
+	else {
+		echo "<p>$connection->error</p>";
+	}
+
 // Closed my connection to my databse
 	$connection->close();

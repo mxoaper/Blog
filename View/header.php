d9391c1972dd83bb244f61721770ee29dbb34078
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-04 23:12:17 -0800

    Added Custom CSS
    I added my own CSS for styling my Blog

--- View/header.php
+++ View/header.php
@@ -4,6 +4,7 @@
  	<meta charset="UTF-8">
 	<title>My BLOG!</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
  </head>
  <body>
 

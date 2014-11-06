9e3803c6bf1e0f661b86b126932aad9caa84e419
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-06 12:13:48 -0800

    Commented out my Code
    I commented out my recently added code to clarify what code does what.

--- View/header.php
+++ View/header.php
@@ -3,8 +3,11 @@
  <head>
  	<meta charset="UTF-8">
 	<title>My BLOG!</title>
+	<!-- BootStrap -->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<!-- My CSS -->
 	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<!-- Allows Media Queries to work! -->
 	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 
  </head>

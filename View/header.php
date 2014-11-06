dfa135f33b4021668555c56c462152eaa11b5a38
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-06 12:11:27 -0800

    Media Query works!!!
    I've added a media query which will make my blog mobile friendly for phones to use. I made the background red to test it out and it was a success!  I also inserted code into my header which lets media queries work.

--- View/header.php
+++ View/header.php
@@ -5,6 +5,8 @@
 	<title>My BLOG!</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+
  </head>
  <body>
 

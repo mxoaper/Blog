4e92310578ccde21605d728173de296cb5907f8c
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-17 09:44:03 -0800

    Created a New Database Class
    I made a new database class with private variables that no other file could access.

--- Model/database.php
+++ Model/database.php
@@ -1,6 +1,11 @@
-<?php
- $host = "localhost";
- $username = "root";
- $password = "root";
- $database = "blog_db"; 
+<?php  
+// No other fule can access these variables because they are private variables
+// We are making a new instance Class to make an Object
+class Database {
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database;
+}
 ?>

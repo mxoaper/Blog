879829a32f7b41944f6aea069d9239da2fefcfd5
Mikael Nortikyan <nortikyan99@gmail.com>
2014-11-17 23:23:10 -0800

    Created a constructor for my database, and functions within my Database Class
    I made a constructor for my database, and I also made 3 functions within my database class, one that opens a connection, another that closes it, and a third for a query.

--- Model/database.php
+++ Model/database.php
@@ -1,5 +1,5 @@
 <?php  
-// No other fule can access these variables because they are private variables
+// No other file can access these variables because they are private variables
 // We are making a new instance Class to make an Object
 class Database {
 	private $connection;
@@ -7,4 +7,25 @@ class Database {
 	private $username;
 	private $password;
 	private $database;
+
+// piece of code that can be reused again and again in substitution for the actual Class
+	public function __construct($host, $username, $password, $database){
+		$this->host = $host;
+		$this->username = $username;
+		$password->password = $password;
+		$this->database = $database;
+
+	}
+// Function that opens the connection
+	public function openConnection(){
+
+	}
+// Fucntion to close the connection
+	public function closeConnection(){
+
+	}
+// Creates a query
+	public function query($string){
+
+	}
 }

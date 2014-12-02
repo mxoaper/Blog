8271c9cb348ef84f8d9e176dcf8a8671fa155860
Mikael Nortikyan <nortikyan99@gmail.com>
2014-12-02 11:59:57 -0800

    Created a Registration Form
    I created a file called register-form.php . There are three DIVs inside it which are to input your email, username, and password. I created the registration form but have not used it yet in my Blog.

---
+++ View/register-form.php
@@ -0,0 +1,25 @@
+<form>
+	<div>
+		<!-- Labled Email and you'll be able to input your email -->
+		<label for="email">Email : </label>
+		<input tpye="text" name="email" />
+	</div>
+
+	<div>
+		<!-- Labeled Username and you'll input your username -->
+		<label for="username">Username : </label>
+		<input type="text" name="username" />
+	</div>
+
+	<div>
+		<!-- Labeled Password and you'll input your password -->
+		<label for="password">Password: </label>
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<!-- Submitting Button -->
+		<button type="submit">Submit</button>
+	</div>
+
+</form>>

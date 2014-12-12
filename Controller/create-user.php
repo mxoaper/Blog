62b525862a09693f730ddf8b65ebcea19aa8b212
Mikael Nortikyan <nortikyan99@gmail.com>
2014-12-11 20:08:00 -0800

    Stored the Register Form Information
    I wrote code that will allow a user to register to my blog, they input their email, password, and username.

--- Controller/create-user.php
+++ Controller/create-user.php
@@ -1,2 +1,13 @@
 <?php 
-	echo "Successfuly Created User";
+	// Requires config.php in this file
+	require_once(__DIR__ . "/../model/config.php");
+
+	//Filters through the email inputted
+	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+	//Filters through the username inputted
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	//Filters through the password inputted
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+	// echoes out the email, username, and password that the user inputted
+	echo $email . " - " . $username . " - " . $password;

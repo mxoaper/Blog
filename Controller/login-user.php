a0ebd57d406fcbd3a5fba9c3977c9debf30c9508
Mikael Nortikyan <nortikyan99@gmail.com>
2014-12-12 00:12:31 -0800

    Selected User Information from the Database
    I wrote code that will select user information from the database. It will select the salt and password from the database and will have future uses as into the user having entered the correct password and having matching salts.

--- Controller/login-user.php
+++ Controller/login-user.php
@@ -1,2 +1,8 @@
 <?php
-	echo "Successfully logged in user";
+	require_once(__DIR__ . "/../model/config.php");
+
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+	// Selects the salt and password form the users table where the username is the username sent via the post 
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WERE username = '$username'");

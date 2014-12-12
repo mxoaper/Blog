bfbfdaf80b42c42b7f481099a4f355ac0aa52ea9
Mikael Nortikyan <nortikyan99@gmail.com>
2014-12-11 22:33:49 -0800

    Encrypted the Password and Storied it in the Database
    I wrote code that encrypts the password by salting and encrypting it so that no one knows the password, an I've made a query which will insert the emails, usernames, and passwords of registering users into the database.

--- Controller/create-user.php
+++ Controller/create-user.php
@@ -9,13 +9,30 @@
 	//Filters through the password inputted
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-	// echoes out the password that the user inputted
-	echo $password;
-
 	// $5$ tells php that we want to use SHA 256 5,000 times to create a unique salt for the user
 	// uniqid is a fucntion that creates a unique id for us
 	// mt_rand generates a random number and uses that number to create a really unique id for it
 	$salt = "$5$" . "round=5000$" . uniqid(mt_rand(), true) . "$";
 
-	// Echoes the salt for the user who registered
-	echo $salt;
+	// Telling the crypt fucntion to use this password and this salt to create an encrypted password
+	$hashedPassword = crypt($password, $salt);
+
+	// Created a query and it will insert into the table called users
+	$query = $_SESSION["connection"]->query("INSERT INTO users SET "
+			// Sets Email to Email variable
+			. "email = '$email',"
+			// Sets Username to Username variable
+			. "username = '$username',"
+			// Sets Password to the hashedPassword variable
+			. "password = '$hashedPassword',"
+			// Sets Salt to the Salt variable
+			. "salt = '$salt'");
+	// Checks if the Query is successful
+	if ($query) {
+		// Echoes out success if successful
+		echo "Successfully created user: $username";
+	}
+	else {
+		// If not successful, it will display the error preventing success
+		echo "<p>" . $_SESSION["connection"]->error . "</p>";
+	}

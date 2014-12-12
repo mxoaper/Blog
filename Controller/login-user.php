aa2a128bd13a8b8647c68341a430eedd6ecf3e35
Mikael Nortikyan <nortikyan99@gmail.com>
2014-12-12 01:21:44 -0800

    Checking the Password for Login
    I wrote code that checks the password when logging in. When logging in to the blog, the user's given information (Username, Password) gets checked with their given information from when they registered which is stored in the database. If the given information is not valid, they are not able to login.

--- Controller/login-user.php
+++ Controller/login-user.php
@@ -1,8 +1,26 @@
 <?php
+	//Requires config.php inside this file
 	require_once(__DIR__ . "/../model/config.php");
 
 	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
 	// Selects the salt and password form the users table where the username is the username sent via the post 
-	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WERE username = '$username'");
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+
+	if($query->num_rows === 1) {
+		$row = $query->fetch_array();
+
+		if ($row["password"] === crypt($password, $row["salt"])) {
+			// Echoes to the user that the login was successful
+			echo "<p>Login Successful!</p>";
+		}		
+		else {
+			// Echoes to the user that his given information wasn't correct
+			echo "<p>Invalid username and password</p>";
+		}
+	}
+	else {
+		// Echoes to the user that his given information wasn't correct
+		echo "<p>Invalid username and password</p>";
+	} 

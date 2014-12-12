a8cc29324afad7f825e45ad87fa4135337348282
Mikael Nortikyan <nortikyan99@gmail.com>
2014-12-11 21:47:51 -0800

    Created a Salt for the Password
    I wrote code which salts the user's password so it can't be easily found out. I used SHA 256 to salt the passwords, and it will give every password a unique salt.

--- Controller/create-user.php
+++ Controller/create-user.php
@@ -9,5 +9,13 @@
 	//Filters through the password inputted
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-	// echoes out the email, username, and password that the user inputted
-	echo $email . " - " . $username . " - " . $password;
+	// echoes out the password that the user inputted
+	echo $password;
+
+	// $5$ tells php that we want to use SHA 256 5,000 times to create a unique salt for the user
+	// uniqid is a fucntion that creates a unique id for us
+	// mt_rand generates a random number and uses that number to create a really unique id for it
+	$salt = "$5$" . "round=5000$" . uniqid(mt_rand(), true) . "$";
+
+	// Echoes the salt for the user who registered
+	echo $salt;

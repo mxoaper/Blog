539f70d99011067670c7550373b977dd8852c140
Mikael Nortikyan <nortikyan99@gmail.com>
2014-12-16 00:26:41 -0800

    Got my Readmore.js to work!!!
    I finally figured out how to make the user have the ability to press Read More to not be flooded with so much text!

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+$('.post').readmore({
+  speed: 75,
+  maxHeight: 500
+});
+
+$('.post').readmore({
+  afterToggle: function(trigger, element, expanded) {
+    if(! expanded) { // The "Close" link was clicked
+      $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
+    }
+  }
+});
--- View/footer.php
+++ View/footer.php
@@ -1,3 +1,8 @@
+		 <!--jQuery hosted by Google CDN -->
 		 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		 <!-- Readmore.min.js -->
+		 <script src="js/readmore.min.js"></script>		 
+		 <!-- Main.JS -->
+		 <script src="js/main.js"></script>
 	</body>
 </html>
